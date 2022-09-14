f92098252b128d1699892f8e17cd095313baaeb7
woutersnowflake <111482282+woutersnowflake@users.noreply.github.com>
2022-09-14 16:18:26 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
-def get_fruityadvice_data(this_fruit_choice);
+def get_fruityadvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityice_normalized
